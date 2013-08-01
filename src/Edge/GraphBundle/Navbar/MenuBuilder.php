27a7950091d994081c3f2e0a1d4c3859a54583e2
David Tabachnikov <nethunter@studiosh.com>
2013-08-01 09:17:57 +0300

    Removed timebased chart

--- src/Edge/GraphBundle/Navbar/MenuBuilder.php
+++ src/Edge/GraphBundle/Navbar/MenuBuilder.php
@@ -12,10 +12,10 @@ class MenuBuilder extends AbstractNavbarMenuBuilder
     public function createMainMenu(Request $request)
     {
         $menu = $this->createNavbarMenuItem();
-        $dropdown = $this->createDropdownMenuItem($menu, "Chart", false, array('caret' => true));
+        /* $dropdown = $this->createDropdownMenuItem($menu, "Chart", false, array('caret' => true));
         $dropdown->addChild('Minutes', array('route' => 'edge_graph_homepage', 'time' => '1min'));
         $dropdown->addChild('Hours', array('route' => 'edge_graph_homepage'));
-        $dropdown->addChild('Days', array('route' => 'edge_graph_homepage'));
+        $dropdown->addChild('Days', array('route' => 'edge_graph_homepage')); */
         $menu->addChild('Last', array('route' => 'edge_graph_homepage_last'));
         $menu->addChild('Predict', array('route' => 'edge_graph_homepage_predict'));
         $menu->addChild('RSS', array('route' => 'edge_graph_homepage_rss'));
